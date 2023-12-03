1340b4582d0b878c26413b476a7f658e6c25c301
MacNikSipeykin <niksipeykin@mail.ru>
2023-12-03 17:35:32 +0300

    Create Spring Boot project.

---
+++ GB_Spring 2/src/test/java/com/gb/spring/lessons/GB_Spring/GbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.spring.lessons.GB_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
